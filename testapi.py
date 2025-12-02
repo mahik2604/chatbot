537781a6f8380b4d4ec21e4063cdbfc760f92197
MahimaYdv0 <64626519+mahik2604@users.noreply.github.com>
2025-12-02 12:28:50 +0530

    Initial commit

---
+++ testapi.py
@@ -0,0 +1,9 @@
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.3,
+)
+
+res = llm.invoke("what you doing?")
+print(res)
